c314203f9948eb691c8fe87975adb385e3472543
pushpakbv <pushpakbv2003@gmail.com>
2024-11-14 15:09:18 +0530

    Trainer completely modified

--- Fullstack_Projects/Trainer_Project/frontend/src/App.jsx
+++ Fullstack_Projects/Trainer_Project/frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+import HomePage from './components/HomePage'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
  
 
   return (
     <>
-      <h1 className='text-8xl'>Hello</h1>
+      <Header/>
+      {/* <HomePage /> */}
+      <Footer/>
     </>
   )
 }
